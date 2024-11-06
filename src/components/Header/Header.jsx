65bbc7411ac614d08980a4cde5d61e064d6f1079
FUFU222 <tnkakr_0817@icloud.com>
2024-11-06 18:28:27 +0900

    global.css追記

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,33 @@
+import Image from "next/image"
+import Link from "next/link"
 import styles from "./Header.module.css"
 
 export default function Header() {
   return (
     <header className={styles.Header}>
-      Header
+      <Image 
+        src="/images/companyLogo.png"
+        alt="株式会社CHAIRMANのロゴ"
+        width={200}
+        height={40}
+        priority
+        />
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/about">About</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/services">Services</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
